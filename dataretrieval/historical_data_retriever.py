1cc5440e8ebb36601b2cd8540919af091a8ba7a8
Sam Upra <samupra@Sams-MacBook-Pro.local>
2017-06-29 01:12:38 +0700

    prefix to suffix

--- dataretrieval/historical_data_retriever.py
+++ dataretrieval/historical_data_retriever.py
@@ -1,5 +1,4 @@
 #Self explanatory :)
-
 from pandas_datareader import data as panda_reader
 import fix_yahoo_finance as yf
 from datetime import datetime, timedelta
@@ -8,14 +7,14 @@ from data_retriever import DataRetriever
 class HistoricalDataRetriever(DataRetriever):
     def __init__(self, path) :
         self.data_path = path
-        self.bkk_stock_prefix = ".BK"
+        self.bkk_stock_suffix = ".BK"
 
     def read_data_as_dataframe(self, stock_name, num_days = 90,
                                source = 'yahoo', path = None) :
         self.__read_from_yahoo(stock_name, num_days)
 
     def __read_from_yahoo(self, stock_name, num_days) :
-        bkk_stock_name = stock_name + self.bkk_stock_prefix
+        bkk_stock_name = stock_name + self.bkk_stock_suffix
 
         end_date = datetime.date.today()
         start_date = end_date - relativedelta(days = num_days)
@@ -24,6 +23,7 @@ class HistoricalDataRetriever(DataRetriever):
                     bkk_stock_name, start = start_date,
                     end=end_date)
 
+        # Returns data with columns : Open High Low Close Adj Close Volume
         stock_data
 
     #Implement data reading from Siamchart.com
