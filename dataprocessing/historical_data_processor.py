0ad4a0073e5b94639e4070c8dbdcfff6ede66fc4
Pongpira Upra <pg.upra@gmail.com>
2017-06-29 01:35:40 +0700

    remove blank space

--- dataprocessing/historical_data_processor.py
+++ dataprocessing/historical_data_processor.py
@@ -2,5 +2,4 @@ from data_retriever import DataProcessor
 class HistoricalDataProcessor(DataProcessor):
     def transform_data(self, dataframe) :
         yahoo_dataframe_columns = list(dataframe.columns.values)
-        
         pass
