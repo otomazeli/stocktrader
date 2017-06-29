f3e725d5474eec3222250dfec129f2d5c361ad11
Sam Upra <samupra@Sams-MacBook-Pro.local>
2017-06-30 02:11:21 +0700

    added data processing part

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main() :
     historical_data_reader = HistoricalDataRetriever()
     historical_data_processor = HistoricalDataProcessor()
     stock_data = historical_data_reader.read_data_as_dataframe(stock_name = 'BEC')
-    print(stock_data)
+    final_data = historical_data_processor.transform_data(stock_data)
+    print(final_data)
 
 main()
